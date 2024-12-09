a696c938cfb6bd95d13f7ea03c106645b3d18928
Grace Tao <gtao1@terpmail.umd.edu>
2024-12-09 16:12:18 -0500

    first commit

---
+++ p4/flask_app/config.py
@@ -0,0 +1,2 @@
+SECRET_KEY = "\x12\xae\x84|\xddB\xb7d\x9a\xd6D'\x17css"
+MONGODB_HOST = "mongodb+srv://username:password@cmsc388jmoviedb.omrh0.mongodb.net/reviews?retryWrites=true&w=majority&appName=CMSC388JMovieDB"
--- tw.sh
+++
@@ -1,1 +0,0 @@
-npx tailwindcss -i ./flask_app/static/input.css -o ./flask_app/static/output.css --watch
